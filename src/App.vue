3ca366ddf00d9c9942b4ab208bee04a633305267
Landon Hatch <landonhatch@gmail.com>
2019-04-19 22:19:52 -0600

    Moved nav to top

--- src/App.vue
+++ src/App.vue
@@ -6,44 +6,19 @@
         <span class="font-weight-light">GraphQL Servers</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
+      <v-btn-toggle v-model="toggle_one" mandatory>
+      <v-btn style="color: #e5e5e5; padding: 0 20px;" large color="info" @click="$router.push('gqlall')">GraphQL Server</v-btn>
+
+      <v-btn style="color: #e5e5e5; padding: 0 20px;" large color="info" @click="$router.push('test')">RESTfull Server</v-btn>
+      </v-btn-toggle>
     </v-toolbar>
+
+
 
     <v-content>
       <router-view></router-view>
     </v-content>
 
-    <v-card height="55px" flat>
-
-    <v-bottom-nav
-      :value="true"
-      fixed
-    >
-
-
-      <v-btn
-        color="teal"
-        flat
-        value="GraphQL Server"
-        @click="$router.push('gqlall')"
-      >
-      
-        <span>GraphQL Server</span>
-        
-      </v-btn>
-
-      <v-btn
-        color="teal"
-        flat
-        value="Test"
-        @click="$router.push('test')"
-      >
-      
-        <span>Test</span>
-        
-      </v-btn>
-
-    </v-bottom-nav>
-  </v-card>
   </v-app>
 </template>
 
@@ -59,13 +34,17 @@ export default {
   },
   data () {
     return {
-
+      text: 'center',
+        icon: 'justify',
+        toggle_one: 0,
     }
   }
 }
 </script>
 
 <style>
-
+h1 {
+  color: #fb8c00;
+}
 </style>
 
